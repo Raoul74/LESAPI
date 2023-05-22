cc1cecd311dbe8ecb02d9529b104c861781bbdf0
unknown <raoulkuete74@gmail.com>
2023-05-22 09:42:42 +0100

    premier commit

---
+++ cours/script.js
@@ -0,0 +1,18 @@
+//XMLHttpRequest
+// function reqListener(){
+//     console.log(this.responseText);
+// }
+
+;
+// req.open("get","data.txt",true);
+// req.open("get","data.json",true);
+// req.open("get","https://api.blablagues.net/?rub=blagues",true);
+// req.send();
+
+
+
+//FETCH
+fetch("monlieu","objetoption")
+.then((response)=>{
+    console.log(response);
+});
